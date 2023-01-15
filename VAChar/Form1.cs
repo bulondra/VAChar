02359f1ea467478f8ab843c22bb38b483fc0b2e2
bulondra <dodisek17@gmail.com>
2023-01-15 16:36:02 +0100

    Generating curves

--- VAChar/Form1.cs
+++ VAChar/Form1.cs
@@ -19,7 +19,14 @@ namespace VAChar
                 new double[] { 0.0, 1.84, 3.35, 6.5, 7.96, 8.09, 8.25, 8.35, 8.53 },
             };
             double[] collectorVoltage = { 0.0, 0.3, 0.5, 1.0, 2.0, 4.0, 6.0, 8.0, 10.0 };
-            Color[] colors = { Color.Blue, Color.Crimson, Color.Green, Color.Fuchsia, Color.Orange};
+
+            double[] inputCurrent = { 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+            double[][] inputVoltage =
+            {
+                new double[] { 0.0, 0.64, 0.67, 0.68, 0.69, 0.71, 0.72, 0.73, 0.74 }
+            };
+            
+            Color[] colors = { Color.Blue, Color.Crimson, Color.Green, Color.Fuchsia, Color.Orange, Color.Purple};
 
             this.Text = "Voltampérová charakteristika bipolárního tranzistoru";
             this.Size = new Size(1380, 780);
@@ -27,24 +34,51 @@ namespace VAChar
             // Vytvoření grafu a přidání dat
             Chart chart = new Chart();
             chart.Size = new Size(1280, 720);
+            chart.Legends.Add("Legendy");
 
             for (int i = 0; i < collectorCurrent.Length; i++)
             {
-                chart.Series.Add("Výstupní charakteristika" + i);
-                chart.Series["Výstupní charakteristika" + i].ChartType = SeriesChartType.Line;
-                chart.Series["Výstupní charakteristika" + i].Color = colors[i];
+                chart.Series.Add("Výstupní charakteristika " + i);
+                chart.Series["Výstupní charakteristika " + i].ChartType = SeriesChartType.Spline;
+                chart.Series["Výstupní charakteristika " + i].Color = colors[i];
             }
+            
+            for (int i = 0; i < inputVoltage.Length; i++)
+            {
+                chart.Series.Add("Vstupní charakteristika " + i);
+                chart.Series["Vstupní charakteristika " + i].ChartType = SeriesChartType.Spline;
+                chart.Series["Vstupní charakteristika " + i].Color = colors[colors.Length -1 - i];
+            }
+            
+            chart.Series.Add("Převodní charakteristika");
+            chart.Series["Převodní charakteristika"].ChartType = SeriesChartType.Line;
+            chart.Series["Převodní charakteristika"].Color = Color.Black;
 
             chart.ChartAreas.Add(new ChartArea("Výstupní charakteristika"));
-            
+
             // Nastavení rozsahu osy X na -10 až 10
-            chart.ChartAreas["Výstupní charakteristika"].AxisX.Minimum = -10;
-            chart.ChartAreas["Výstupní charakteristika"].AxisX.Maximum = 10;
+            chart.ChartAreas["Výstupní charakteristika"].AxisX.Minimum = -11;
+            chart.ChartAreas["Výstupní charakteristika"].AxisX.Maximum = 11;
 
             // Nastavení rozsahu osy Y na -10 až 10
-            chart.ChartAreas["Výstupní charakteristika"].AxisY.Minimum = -10;
-            chart.ChartAreas["Výstupní charakteristika"].AxisY.Maximum = 10;
+            chart.ChartAreas["Výstupní charakteristika"].AxisY.Minimum = -11;
+            chart.ChartAreas["Výstupní charakteristika"].AxisY.Maximum = 11;
             
+            chart.ChartAreas[0].AxisX.MajorGrid.Enabled = true;
+            chart.ChartAreas[0].AxisY.MajorGrid.Enabled = true;
+            
+            chart.ChartAreas[0].AxisX.Interval = 1;
+            chart.ChartAreas[0].AxisY.Interval = 1;  
+            
+            chart.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.Silver;
+            chart.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.Silver;
+            
+            chart.ChartAreas[0].AxisX.Title = "Ib   |   Uce";
+            chart.ChartAreas[0].AxisY.Title = "Ube  |   Ic";
+            
+
+
+            double yy = 0.0;
             int currentId = 0;
             foreach (double[] current in collectorCurrent)
             {
@@ -57,13 +91,47 @@ namespace VAChar
                     double y = spline.Interpolate(current[i]);
                     
                     //chart.Series["Výstupní charakteristika" + currentId].Points.AddXY(x, y);
-                    chart.Series["Výstupní charakteristika" + currentId].Points.AddXY(collectorVoltage[i], current[i]);
+                    chart.Series["Výstupní charakteristika " + currentId].Points.AddXY(collectorVoltage[i], current[i]);
+                    yy = y;
                 }
 
                 currentId++;
             }
+
+            double xx = 0.0;
             
+            int voltageId = 0;
+            foreach (double[] voltage in inputVoltage)
+            {
+                for (int i = 0; i < voltage.Length; i++)
+                {
+                    //var spline = CubicSpline.InterpolateNatural(collectorVoltage, voltage);
+                    
+                    /*
+                    double x = spline.Interpolate(collectorVoltage[i]);
+                    
+                    double y = spline.Interpolate(voltage[i]);
+                    */
+
+                    double x = inputCurrent[i] * -1;
+                    double y = voltage[i] * -1;
+                    
+                    //chart.Series["Výstupní charakteristika" + currentId].Points.AddXY(x, y);
+                    chart.Series["Vstupní charakteristika " + voltageId].Points.AddXY(x/10, y);
+
+                    xx = x;
+                }
+
+                voltageId++;
+            }
+
+            chart.Series["Převodní charakteristika"].Points.AddXY(0, 0);
             
+            double[] highestCurrent = collectorCurrent[collectorCurrent.Length - 1];
+            chart.Series["Převodní charakteristika"].Points.AddXY(xx/10, yy);
+            
+
+
             // Přidání grafu do formuláře a zobrazení
             this.Controls.Add(chart);
         }
